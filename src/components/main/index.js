84d45a205eb4bfe4bb633507dd194527c707bef7
Ben Merchant <benjaminmerchant@gmail.com>
2019-03-22 00:46:15 -0400

    close enough for government work

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -19,18 +19,19 @@ import express from 'express';
 // };
 
 // try the old way for modules
-
+// This is a functions that accepts the express app object
+// so that we can mount ONE Middleware on it
+// that Middleware carries others with it
 module.exports = function(app) {
 
   const apiRoutes = express.Router();
 
-
   const mainRouter = express.Router();
-
   apiRoutes.use('/main', mainRouter);
-
   mainRouter.get('/', (req, res, next) => {
     res.status(200).json({message: 'Welcome to the site!'});
   });
+
+
   app.use('/api', apiRoutes);
 };
